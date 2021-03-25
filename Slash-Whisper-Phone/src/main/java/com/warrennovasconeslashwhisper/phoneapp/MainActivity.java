1dc933ef3ecc3b56c13c2ec6473be3b69393b734
Novascone <novasconewarren@gmail.com>
2021-03-25 16:50:59 -0600

    SpeechToText

--- Slash-Whisper-Phone/src/main/java/com/warrennovasconeslashwhisper/phoneapp/MainActivity.java
+++ Slash-Whisper-Phone/src/main/java/com/warrennovasconeslashwhisper/phoneapp/MainActivity.java
@@ -3,17 +3,104 @@ package com.warrennovasconeslashwhisper.phoneapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 
 import com.warrennovasconeslashwhisper.api.Verify;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     public static final int RECORD_CODE = 1;
+    private boolean talking = false;
+//    TextToSpeech textToSpeech;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Verify.verifyPhoneApp();
         requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO},RECORD_CODE);
+//        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+//            @Override
+//            public void onInit(int status) {
+//                textToSpeech.setLanguage(Locale.getDefault());
+//            }
+//        });
+
+        SpeechRecognizer recognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        recognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+                System.out.println("Error: " + error);
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> result = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                String text = result.get(0);
+                //textToSpeech.speak("", TextToSpeech.QUEUE_FLUSH, null,"text");
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+        Intent recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        recognizer.startListening(recognizerIntent);
+
+        findViewById(R.id.speech).setOnClickListener((view) -> {
+            if(!talking)
+            {
+                recognizer.startListening(recognizerIntent);
+                talking = true;
+            }else{
+                talking = false;
+                recognizer.stopListening();
+            }
+        });
+
     }
+
+
 }
